2cab18fea4c0894c8ed67e59196f47709f18fd87
Tahatosun <35890316+Tahatosun@users.noreply.github.com>
2021-02-28 16:36:50 +0300

    Task Completed

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-import { getDataFromApi, addTaskToApi } from './data';
+import { getDataFromApi, addTaskToApi, deleteTaskToApi } from './data';
 
 class PomodoroApp {
   constructor(options) {
@@ -6,19 +6,29 @@ class PomodoroApp {
     this.$tableTbody = document.querySelector(tableTbodySelector);
     this.$taskForm = document.querySelector(taskFormSelector);
     this.$taskFormInput = this.$taskForm.querySelector('input');
+    this.$taskAddButton = this.$taskForm.querySelector('button');
+  }
+
+  toggleDisable() {
+    this.$taskAddButton.innerText =
+      this.$taskAddButton.innerText == 'Add Task' ? 'Loading...' : 'Add Task';
+    this.$taskAddButton.disabled = this.$taskAddButton.disabled ? false : true;
   }
 
   addTask(task) {
+    this.toggleDisable();
     addTaskToApi(task)
       .then((data) => data.json())
       .then((newTask) => {
         this.addTaskToTable(newTask);
+        this.toggleDisable();
       });
   }
 
   addTaskToTable(task, index) {
     const $newTaskEl = document.createElement('tr');
-    $newTaskEl.innerHTML = `<th scope="row">${task.id}</th><td>${task.title}</td>`;
+    $newTaskEl.setAttribute('data-id', task.id);
+    $newTaskEl.innerHTML = `<th scope="row">${task.id}</th><td>${task.title}</td><td><button type="button" data-id="${task.id}" class="delete btn btn-danger"><i data-id="${task.id}" class="fa fa-trash delete"></i></button></td>`;
     this.$tableTbody.appendChild($newTaskEl);
     this.$taskFormInput.value = '';
   }
@@ -39,9 +49,26 @@ class PomodoroApp {
     });
   }
 
+  handleDeleteTask() {
+    document
+      .getElementById('table-tbody')
+      .addEventListener('click', (event) => {
+        if (event.target.className.includes('delete')) {
+          let dataID = event.target.getAttribute('data-id');
+          console.log(dataID);
+          deleteTaskToApi(dataID).then(
+            event.currentTarget
+              .querySelector(`tr[data-id='${dataID}']`)
+              .remove()
+          );
+        }
+      });
+  }
+
   init() {
     this.fillTasksTable();
     this.handleAddTask();
+    this.handleDeleteTask();
   }
 }
 
--- src/constans.js
+++ src/constans.js
@@ -1,1 +1,1 @@
-export const API_URL = 'https://6032450ea223790017aced0a.mockapi.io/tasks';
+export const API_URL = 'https://6037795554350400177227d4.mockapi.io/tasks';
